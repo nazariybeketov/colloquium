8e47fb201ab64eb8961579bd13895b19be225cf6
Nazariy <nazarijjjjjj@gmail.com>
2024-02-12 01:38:22 +0300

    STARTED TO BEAUTIFYING

--- main.py
+++ main.py
@@ -1,47 +1,33 @@
 """Тестовое задание"""
 
-
-def welcome():
-    counter = 0
-    print(f"Здравствуйте\n")
-    which_cmd = [
-        "Вывод постранично записей из справочника на экран",
-        "Добавление новой записи в справочник",
-        "Редактирования записи в справочнике",
-        "Поиск записей по одной или нескольким характеристикам",
-    ]
-
-    for i in range(4):
-        print(f"{i+1} - {which_cmd[i]}")
-
-    print("\n")
-    user_ans = int(input("Какой командой желаете восьпользоваться? => "))
-
-    if user_ans == 1:
-        page_by_page()
-
-    elif user_ans == 2:
-        add_human()
-    elif user_ans == 3:
-        edit_contact()
-
-    elif user_ans == 4:
-        s = find_str()
-        for i in range(len(s)):
-            print(s[i])
+human_params = [
+    "Фамилия",
+    "Имя",
+    "Отчество",
+    "Название организации",
+    "Телефон рабочий",
+    "Телефон личный (сотовый)",
+]
 
 
-def add_human():
-    with open(file="catalog.txt", encoding="utf-8", mode="a+") as catalog:
-        human_info = input("Вводите данные: ").split()
-        for info in human_info:
-            if len(catalog.readline()) > 0:
-                catalog.write(f"\n")
-                catalog.write(f"{info} ")
-            else:
-                catalog.write(f"{info} ")
+# Добавление пользователя в записную книжку
+def add_human(human_params=human_params):
 
-        catalog.write("\n")
+    human = []
+
+    for param in human_params:
+        human.append(input(f'Введите поле "{param.upper()}" => '))
+
+    with open(file="catalog.txt", mode="a+", encoding="utf-8") as catalog:
+
+        catalog.seek(0, 2)
+
+        if len(catalog.readlines()) > 0:
+            catalog.write("\n")
+
+        catalog.write(" ".join(human))
+
+        print(f"\nКОНТАКТ УСПЕШНО ДОБАВЛЕН!!!\n")
 
 
 def find_str(counter=0):
@@ -49,14 +35,6 @@ def find_str(counter=0):
     response = []
 
     ###################
-    finding_options = [
-        "Фамилия",
-        "Имя",
-        "Отчество",
-        "Название организации",
-        "Телефон рабочий",
-        "Телефон личный (сотовый)",
-    ]
 
     print("По какому признаку будем искать?\n")
     for i in range(6):
@@ -128,6 +106,36 @@ def page_by_page():
             file=f"one_record_{i+1}.txt", encoding="utf-8", mode="w"
         ) as temp_file:
             temp_file.write(line[i])
+
+
+def welcome():
+    counter = 0
+    print(f"Здравствуйте\n")
+    which_cmd = [
+        "Вывод постранично записей из справочника на экран",
+        "Добавление новой записи в справочник",
+        "Редактирования записи в справочнике",
+        "Поиск записей по одной или нескольким характеристикам",
+    ]
+
+    for i in range(4):
+        print(f"{i+1} - {which_cmd[i]}")
+
+    print("\n")
+    user_ans = int(input("Какой командой желаете восьпользоваться? => "))
+
+    if user_ans == 1:
+        page_by_page()
+
+    elif user_ans == 2:
+        add_human()
+    elif user_ans == 3:
+        edit_contact()
+
+    elif user_ans == 4:
+        s = find_str()
+        for i in range(len(s)):
+            print(s[i])
 
 
 welcome()
