18827042768aab5703214e9dbc45506bab5d896b
Nazariy <nazarijjjjjj@gmail.com>
2024-02-12 21:03:12 +0300

    already beautiful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """Тестовое задание"""
 
-human_params = [
+from typing import NoReturn
+
+
+human_params: list[str] = [
     "Фамилия",
     "Имя",
     "Отчество",
@@ -10,8 +13,44 @@ human_params = [
 ]
 
 
-# Добавление пользователя в записную книжку
-def add_human(human_params=human_params):
+def welcome() -> NoReturn:
+    counter = 0
+    print(f"Здравствуйте\n")
+    which_cmd = [
+        "Вывод постранично записей из справочника на экран",
+        "Добавление новой записи в справочник",
+        "Редактирования записи в справочнике",
+        "Поиск записей по одной или нескольким характеристикам",
+        "Выйти",
+    ]
+
+    for i in range(5):
+        print(f"{i+1} - {which_cmd[i]}")
+
+    print("\n")
+    user_ans = int(input("Какой командой желаете восьпользоваться? => "))
+
+    match user_ans:
+        case 1:
+            page_by_page()
+
+        case 2:
+            add_human()
+        case 3:
+            edit_contact()
+
+        case 4:
+            s = find_str()
+            for i in range(len(s)):
+                print(s[i])
+        case 5:
+            ...
+        case _:
+            print("Такой команды не существует :9")
+
+
+# DONE!
+def add_human(human_params: list[str] = human_params) -> str:
 
     human = []
 
@@ -30,20 +69,23 @@ def add_human(human_params=human_params):
         print(f"\nКОНТАКТ УСПЕШНО ДОБАВЛЕН!!!\n")
 
 
-def find_str(counter=0):
+# DONE!
+def find_str(
+    response: list[str] = [],
+    counter: int = 0,
+    finding_options: list[str] = human_params,
+) -> list[str]:
 
-    response = []
-
-    ###################
+    ################### Вывод вариантов ответов
 
     print("По какому признаку будем искать?\n")
     for i in range(6):
         print(f"{i+1} - {finding_options[i]}")
 
     print("\n")
-    ################### Вывод вариантов ответов
+    ###################
 
-    # Ответ на то, какой вариант поиска нужен
+    # Ответ на то, какой вариант/ы поиска нужен/ы
     user_answer = list(
         map(int, input("Введите через пробел вариант/ы поиска => ").split())
     )
@@ -52,9 +94,9 @@ def find_str(counter=0):
     desired_answeres = dict()
 
     # Заполнение словаря
-    for temp in user_answer:
-        ans = input(f'Введите поле "{finding_options[temp-1]}" => ')
-        desired_answeres[finding_options[temp - 1]] = ans
+    for k in user_answer:
+        ans = input(f'Введите поле "{finding_options[k-1]}" => ')
+        desired_answeres[finding_options[k - 1]] = ans
 
     # Работа с файлом
     with open(file="catalog.txt", encoding="utf-8", mode="r") as catalog:
@@ -75,67 +117,59 @@ def find_str(counter=0):
     return response
 
 
-######################## Доделать!!!!!!!!!!!!!!!!
-def edit_contact():
+# DONE!
+def edit_contact(human_params: list[str] = human_params) -> NoReturn:
 
-    lines = find_str()
-    if len(lines) > 1:
+    line = find_str()
+
+    if len(line) == 1:
+        print(f"Что хотите изменить?\n")
+        for i in range(len(human_params)):
+            print(f"{i} - {human_params[i]}")
+        print("\n")
+
+        user_answer = list(
+            map(
+                int,
+                input("Введите через пробел поля, которые нужно заменить => ").split(),
+            )
+        )
+        new_line = line[0].split()
+        for j in user_answer:
+            new_line[j] = input(f'Введите поле "{human_params[j]}" => ')
+
+        new_line = " ".join(new_line) + "\n"
+
+        with open(file="catalog.txt", encoding="utf-8", mode="r") as catalog:
+            list_of_catalog = catalog.readlines()
+            for i in range(len(list_of_catalog)):
+                if list_of_catalog[i].strip() == line[0].strip():
+                    list_of_catalog[i] = new_line
+                    break
+
+        with open(file="catalog.txt", encoding="utf-8", mode="w") as catalog:
+            catalog.writelines(list_of_catalog)
+
+    elif len(line) > 1:
         print(
             "Под данное описание подходит несколько контактов, если хотите изменить какую-то запись укажите более подробное описание!"
         )
         edit_contact()
 
     else:
-        temp_str = input("Вводите данные: ") + "\n"
-
-        with open(file="catalog.txt", encoding="utf-8", mode="r") as catalog:
-            list_of_catalog = catalog.readlines()
-            for i in range(len(list_of_catalog)):
-                if list_of_catalog[i].strip() == lines[0].strip():
-                    list_of_catalog[i] = temp_str
-                    break
-
-        with open(file="catalog.txt", encoding="utf-8", mode="w") as catalog:
-            catalog.writelines(list_of_catalog)
+        print(f"Ни одной записи не нашлось :(\n")
+        print(f"Попробуйте найти с другими параметрами!\n")
+        find_str()
 
 
-def page_by_page():
+# DONE!
+def page_by_page() -> NoReturn:
     line = find_str()
     for i in range(len(line)):
         with open(
             file=f"one_record_{i+1}.txt", encoding="utf-8", mode="w"
         ) as temp_file:
             temp_file.write(line[i])
-
-
-def welcome():
-    counter = 0
-    print(f"Здравствуйте\n")
-    which_cmd = [
-        "Вывод постранично записей из справочника на экран",
-        "Добавление новой записи в справочник",
-        "Редактирования записи в справочнике",
-        "Поиск записей по одной или нескольким характеристикам",
-    ]
-
-    for i in range(4):
-        print(f"{i+1} - {which_cmd[i]}")
-
-    print("\n")
-    user_ans = int(input("Какой командой желаете восьпользоваться? => "))
-
-    if user_ans == 1:
-        page_by_page()
-
-    elif user_ans == 2:
-        add_human()
-    elif user_ans == 3:
-        edit_contact()
-
-    elif user_ans == 4:
-        s = find_str()
-        for i in range(len(s)):
-            print(s[i])
 
 
 welcome()
